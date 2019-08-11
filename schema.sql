c4e197e942877d0aebc1c7c1ccb3d790310dfedd
Angel Rivas <angel@prodigycode.com>
2019-08-10 20:49:11 -0500

    created database table and added items

---
+++ schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS bamazon;
+CREATE DATABASE bamazon;
+USE bamazon;
+
+CREATE TABLE products(
+item_id INT AUTO_INCREMENT NOT NULL,
+product_name VARCHAR(30) NOT NULL,
+department_name VARCHAR(20) NOT NULL,
+price decimal(5,2) not null,
+stock_qty int(5) not null,
+primary key (item_id)
+);
+
+
+SELECT * FROM bamazon.products;
+
+INSERT INTO products(ProductName, DepartmentName, Price, StockQty)
+VALUES("PS4", "Video Games", 300.00, 20),
+	("FIFA 20", "Video Games", 60.00, 100),
+    ("Vacuum", "Home Goods", 75.60, 200),
+    ("The Lion King", "Movies", 29.75, 10),
+    ("Sorround Sound System", "Electronics", 2999.75, 35),
+    ("Protein", "Sports Nutrition", 59.75, 200),
+    ("Printer", "Office Products", 269.99, 30),
+    ("50 inch TV", "Electronics", 799.95, 5),
+    ("Rechargable Baterries", "Electronics", 9.75, 1000),
+    ("Rayban Shades", "Apparel", 285.60, 300);
+    
+SELECT * FROM bamazon.products;
