f31f9e639b2dd4976dbdd7abed9875d8c5acb988
Angel Rivas <angel@prodigycode.com>
2019-08-10 22:35:21 -0500

    instfixed logic bug that wouldn't load table

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,10 +1,25 @@
+
 var mysql = require('mysql');
 var inquirer = require('inquirer');
 
-var conection = mysql.createConnection({
+var connection = mysql.createConnection({
     host:"localhost",
     port:8889,
     user:"root",
     password:"root",
     database:"bamazon"
 })
+
+connection.connect(function(err){
+    if (err) throw err;
+    console.log("connection succesful");
+    loadTable();
+})
+
+var loadTable = function(){
+    connection.query("SELECT * FROM products", function(err,res){
+        if (err) throw err;
+            console.table(res);
+        
+    })
+}
